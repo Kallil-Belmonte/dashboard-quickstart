5044bf35a1239b1f26db18ee621d7d82050633c7
Kallil Belmonte <kallil.belmonte@gmail.com>
2018-12-08 13:12:57 -0200

    Files updated.

---
+++ js/javascript.js
@@ -0,0 +1,72 @@
+//==============================
+// GLOBAL VARIABLES
+//==============================
+var menuHeight = $('.main-menu').outerHeight();
+
+
+//==============================
+// THEME FUNCTIONS
+//==============================
+const themeFunctions = {
+
+  // TOOLTIPS
+  tooltips: function() {
+    $(function() {
+      $('[data-toggle="tooltip"]').tooltip();
+    })
+  },
+
+
+  // TOP MENU
+  topMenu: function() {
+    var menuItem = $('.top-menu .menu > li > a');
+
+    $(menuItem).click(function() {
+      $(this).parent().toggleClass('active').siblings().removeClass('active').parent().next().find('.profile-area').removeClass('active');
+    });
+
+    $('.top-menu .profile-area a').click(function() {
+      $(this).parent().parent().prev().find('li').removeClass('active');
+    });
+
+    $(window).click(function(event) {
+      var target = $(event.target);
+
+      if(!target.is(menuItem)) {
+        $('.top-menu .menu li').removeClass('active');
+      }
+    });
+  },
+
+
+  // HAMBURGER
+  hamburger: function() {
+    $('.hamburger').click(function() {
+      $(this).toggleClass('is-active');
+
+      $('.side-menu').toggleClass('open-side-menu');
+    });
+
+    // Menu
+    $('.side-menu .menu > li > a').click(function() {
+      $(this).toggleClass('active');
+    });
+  },
+
+
+  // INIT
+  init: function() {
+    this.tooltips();
+    this.topMenu();
+    this.hamburger();
+  }
+
+}
+
+
+//==============================
+// THEME FUNCTIONS CALL
+//==============================
+$(document).ready(function() {
+  themeFunctions.init();
+});
